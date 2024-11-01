1b9f2af1045fa4a27de42f54a8450cb037e1d1f4
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-11-01 21:38:57 +0700

    Recent update on Main branch

--- main_character.gd
+++ main_character.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 var speed 
 const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
+const SPRINT_SPEED = 7.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
 var gravity = 9.8
