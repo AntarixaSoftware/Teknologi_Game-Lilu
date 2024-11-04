b8bb5dfa77c2512758936348e492c994c3329751
dwiarthurrevangga <dwiarthurrevangga59@gmail.com>
2024-11-04 16:46:48 +0700

    update

--- main_menu.gd
+++ main_menu.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_scene.tscn")
 
-func _on_option_button_pressed() -> void:
+func _on_setting_button_pressed() -> void:
 	pass # Replace with function body.
 
 
