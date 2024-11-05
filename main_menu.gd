3f7a38239b568832523ba5d3e912bf21ed71b1d6
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-11-05 11:29:10 +0700

    Fixed and merged

--- main_menu.gd
+++ main_menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://main_scene.tscn")
 func _on_setting_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/setting_menu.tscn")
 func _on_quit_button_pressed() -> void:
