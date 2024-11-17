1837c123b73ad37a817c1447fa0c567d17eca1b4
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-11-17 22:29:18 +0700

    Commit

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,33 @@
+class_name MainMenu
 extends Control
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
-func _on_setting_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/setting_menu.tscn")
-func _on_quit_button_pressed() -> void:
+@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var setting_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Setting_Button as Button
+@onready var quit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Quit_Button as Button
+@onready var setting_menu: SettingMenu = $Setting as SettingMenu
+@onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
+@onready var start_level = preload("res://Scenes/main_scene.tscn") as PackedScene
+
+func _ready():
+	handle_connecting_signals()
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+func on_setting_pressed() -> void:
+	margin_container.visible = false
+	setting_menu.set_process(true)
+	setting_menu.visible = true
+
+func on_quit_pressed() -> void:
 	get_tree().quit()
+
+func on_exit_setting_menu() -> void:
+	margin_container.visible = true
+	setting_menu.visible = false
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	setting_button.button_down.connect(on_setting_pressed)
+	quit_button.button_down.connect(on_quit_pressed)
+	setting_menu.exit_setting_menu.connect(on_exit_setting_menu)
