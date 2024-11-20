7409468192f7798e477ac45272e5d50c093634d5
Zahran812 <zahranalbara08@gmail.com>
2024-11-20 13:51:22 +0700

    alam lain
    udah fix sukses

--- main.gd
+++ main.gd
@@ -4,7 +4,14 @@ func _ready():
 	print("Scene ready, applying saved state...")
 	var saved_state = GameState.load_state()
 	if saved_state.has("player_position"):
-		var player = get_node("Main Character")
+		var last_position = saved_state["player_position"]
+		
+		last_position.x -= 0.1
+		last_position.z -= 0.1
+		
+		var player = get_tree().current_scene.get_node("Main Character")
 		if player:
-			print("Setting player position to:", saved_state["player_position"])
-			player.global_transform.origin = saved_state["player_position"]
+			player.global_transform.origin = last_position
+			print("Player position updated to:", last_position)
+		else:
+			print("Error: Player node tidak ditemukan di main scene.")
