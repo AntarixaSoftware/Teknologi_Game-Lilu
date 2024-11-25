b3c7ce5f889f8e29f624215d2d02294ee666bc90
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-11-25 07:04:50 +0700

    init

--- addons/zylann.hterrain/hterrain_data.gd
+++ addons/zylann.hterrain/hterrain_data.gd
@@ -1843,3 +1843,4 @@ static func convert_float_heightmap_to_rgb8(src: Image, dst: Image):
 		for x in src.get_width():
 			var h = src.get_pixel(x, y).r
 			dst.set_pixel(x, y, encode_height_to_rgb8_unorm(h))
+
