b058d60e7abe18cd8d747b0c730c3c823ef5c1da
Zahran812 <167998013+Zahran812@users.noreply.github.com>
2024-11-28 21:44:27 +0700

    Alam lain
    portal cuma sekali

--- GameState.gd
+++ GameState.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var main_scene_state = {}
+var entered : bool = false
 
 func save_state(player_position : Vector3, current_scene_name : String):
 	main_scene_state["player_position"] = player_position
--- portal.gd
+++ portal.gd
@@ -1,15 +1,19 @@
 extends Area3D
 
 @export var alam_lain_path : String = "res://Scenes/alam_lain.tscn"
-@export var main_scene : Node 
+@export var main_scene : Node
+var entered : int = 0
 # Called when the node enters the scene tree for the first time.
 
 	
 func _on_body_entered(body):
-	if body.name == "Main Character":
+	if body.name == "Main Character" and not GameState.entered:
 		GameState.save_state(body.global_transform.origin, get_tree().current_scene.name)
-		
+		GameState.entered = true
 		call_deferred("change_scene_to_alam_lain")
+	else:
+		print("udah masuk")
+		
 
 func change_scene_to_alam_lain():
 	var new_scene = load(alam_lain_path) as PackedScene
