006aeb56faebe67aba0bc15726c83023bd46a166
Zahran812 <167998013+Zahran812@users.noreply.github.com>
2024-11-30 18:35:35 +0700

    waypoint
    waypoint

---
+++ Main_Branch/Scenes/waypoint.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var cam = Camera3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cam = get_viewport().get_camera_3d()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var marker_pos = self.global_transform.origin
+	var camera_dir = -cam.global_transform.basis.z
+	var to_marker_dir = (marker_pos - cam.global_transform.origin).normalized()
+	
+	var dot_product = camera_dir.dot(to_marker_dir)
+	
+	if dot_product > 0:  # Waypoint is in front of the camera
+		var screen_pos = cam.unproject_position(marker_pos)
+		$origin.visible = true
+		$origin.position = screen_pos
+	else:  # Waypoint is behind the camera
+		$origin.visible = false
