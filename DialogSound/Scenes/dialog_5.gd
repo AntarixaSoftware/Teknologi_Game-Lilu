d15f81acf91b89adb504cc116e6a4cd18976b12d
Zahran812 <167998013+Zahran812@users.noreply.github.com>
2024-12-02 12:21:35 +0700

    folder
    .

---
+++ DialogSound/Scenes/dialog_5.gd
@@ -0,0 +1,56 @@
+extends Node3D
+
+@onready var rich_text_label = $RichTextLabel
+@onready var timer = $Timer
+# Variabel daftar dialog
+var dialog_queue: Array = [
+	{"name": "Charles", "dialog": "Ya ampun, gelap kali gua ini. Seram pula... Ucok, kau lihat itu? Ada bekas perdukunan di sini!"
+},
+	{"name": "Charles", "dialog": "Aku yakin ini kerjaan si dukun itu. Bisa jadi ini yang membuat rohku tersesat. Aduh, jahat kali si Tagor ini!"}
+]
+
+var current_name: String = ""
+var current_dialog: String = ""
+var display_text: String = ""  # Teks yang muncul satu per satu
+var char_index: int = 0  # Indeks untuk huruf
+var dialog_index: int = 0  # Indeks untuk antrean dialog
+var char_interval: float = 0.1
+
+func _ready():
+	var start = get_node("/root/Main Scene/Story/Quests/Cave/cave")
+	start.connect("display5", Callable(self, "_on_display5"))
+	
+func _on_display5():
+	rich_text_label.visible = true
+	timer.wait_time = char_interval
+	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+	_load_next_dialog()
+
+func _load_next_dialog():
+	# Pastikan ada dialog di antrean
+	if dialog_index < dialog_queue.size():
+		# Ambil nama dan dialog berikutnya
+		var entry = dialog_queue[dialog_index]
+		current_name = entry["name"]
+		current_dialog = entry["dialog"]
+		display_text = ""
+		char_index = 0
+		
+		rich_text_label.bbcode_enabled = true
+		rich_text_label.text = "[b]" + current_name + ": [/b]"
+
+		# Perbarui indeks antrean
+		dialog_index += 1
+		timer.start()  # Mulai menampilkan dialog baru
+	else:
+		print("All dialogs displayed.")
+		timer.stop()
+
+func _on_timer_timeout():
+	if char_index < current_dialog.length():
+		display_text += current_dialog[char_index]
+		rich_text_label.text = "[b]" + current_name + ": [/b]" + display_text
+		char_index += 1
+	else:
+		timer.stop()
+		_load_next_dialog()
--- Main_Branch/Scripts/Quests/alam_lain.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-@export var musik : AudioStreamPlayer
-
-func _ready() -> void:
-	if musik:
-		musik.play()
