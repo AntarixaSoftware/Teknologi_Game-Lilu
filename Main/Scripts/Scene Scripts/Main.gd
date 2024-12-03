83cb52ee290a918a4dc5824906b74f2963621846
YohanCP <145282836+YohanCP@users.noreply.github.com>
2024-12-03 18:44:42 +0700

    commit

--- Main/Scripts/Scene Scripts/Main.gd
+++ Main/Scripts/Scene Scripts/Main.gd
@@ -1,11 +1,22 @@
-extends Node3D
+extends Node3D  # atau tipe node root lainnya
 
+func _ready():
+	$"Scary-forest-90162".play()
+	if not GameState.entered:
+		_animation()
+	var saved_state = GameState.load_state()
+	if saved_state.has("player_position"):
+		var last_position = saved_state["player_position"]
+		
+		last_position.x -= 0.1
+		last_position.z -= 0.1
+		
+		var player = get_tree().current_scene.get_node("Main Character")
+		if player:
+			player.global_transform.origin = last_position
+			print("Player position updated to:", last_position)
+		else:
+			print("Error: Player node tidak ditemukan di main scene.")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _animation():
+	$Truck/AnimationPlayer.play("TruckCrash")
